3ef747ba0ccc86866ad9952e895dbbc7557483cb
MrBartek302 <126457666+MrBartek302@users.noreply.github.com>
2023-03-13 19:56:08 +0100

    losowanie

--- klient/app.js
+++ klient/app.js
@@ -6,17 +6,42 @@ function zacznij(){
     div_kandydat1.setAttribute("id", "uzytkownik1")
     div_kandydat1.innerHTML = "Kandydat Nr.1"
     const div1_zdj = document.createElement("img")
-    div1_zdj.setAttribute("src", )
+    div1_zdj.setAttribute("src","https://www.ubutik.pl/img/cms/kiedy%20okulary%20dla%20nauczyciela.jpg" )
+    div1_zdj.classList.add("zdjecia")
+    div_kandydat1.appendChild(div1_zdj)
+    const button1 = document.createElement("button")
+    button1.setAttribute("onclick", "wybierz")
+    button1.classList.add("wybieranie")
+    button1.innerHTML = "Wybierz!"
+    div_kandydat1.appendChild(button1)
 
     const div_kandydat2 = document.createElement("div")
     div_kandydat2.classList.add("uzytkownicy")
     div_kandydat2.setAttribute("id", "uzytkownik1")
     div_kandydat2.innerHTML = "Kandydat Nr.2"
+    const div2_zdj = document.createElement("img")
+    div2_zdj.setAttribute("src"," https://st.depositphotos.com/1637787/3445/i/600/depositphotos_34453787-stock-photo-businessman-portrait.jpg" )
+    div2_zdj.classList.add("zdjecia")
+    div_kandydat2.appendChild(div2_zdj)
+    const button2 = document.createElement("button")
+    button2.setAttribute("onclick", "wybierz")
+    button2.classList.add("wybieranie")
+    button2.innerHTML = "Wybierz!"
+    div_kandydat2.appendChild(button2)
 
     const div_kandydat3 = document.createElement("div")
     div_kandydat3.classList.add("uzytkownicy")
     div_kandydat3.setAttribute("id", "uzytkownik1")
     div_kandydat3.innerHTML = "Kandydat Nr.3"
+    const div3_zdj = document.createElement("img")
+    div3_zdj.setAttribute("src","https://st2.depositphotos.com/2501025/5654/i/450/depositphotos_56545139-stock-photo-password.jpg" )
+    div3_zdj.classList.add("zdjecia")
+    div_kandydat3.appendChild(div3_zdj)
+    const button3 = document.createElement("button")
+    button3.setAttribute("onclick", "wybierz")
+    button3.classList.add("wybieranie")
+    button3.innerHTML = "Wybierz!"
+    div_kandydat3.appendChild(button3)
 
     document.getElementById("prawogra").appendChild(div_kandydat1)
     document.getElementById("prawogra").appendChild(div_kandydat2)
