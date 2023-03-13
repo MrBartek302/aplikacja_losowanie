8bf606490f2400b444fe81f517ce1a4ea6a7a5bf
MrBartek302 <126457666+MrBartek302@users.noreply.github.com>
2023-03-13 11:03:38 +0100

    losowanie

---
+++ serwer/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+ const cors = require('cors')
+ const mysql = require('mysql')
+
+const app = express()
+
+app.use(cors())
+
+const port = 3000
+//łączymy się z bazą danych
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "losowanie"
+})
+//potwierdzenie połączenia
+con.connect(function(err){
+    if(err) console.log(err)
+    else console.log("Połączono z bazą danych")    
+})
+
+//pobieramy z bazy danych
+app.get("/pobierz", (req,res)=>{
+    //pobiera nam z naszej bazy danych to co stworzyliśmy
+    const sql = "SELECT * FROM losowanie"
+    //tworzymy zapytanie
+    con.query(sql, function(err, result, fields){
+        if(err) console.log(err)
+        else res.send(result)
+    })
+})
+
+//SELECT pobiera a INSERT dodaje
+
+//wysyłamy do bazy danych
+app.get("/add/:wylosowany/:pesel", (req,res)=>{
+    const pesel = req.params.pesel
+    const wylosowany = req.params.wylosowany
+    //tworzymy zapytanie
+    const sql = `INSERT INTO losowanie (wylosowany,pesel) VALUES ('${wylosowany}', '${pesel}')`
+    con.query(sql, function(err, result, fields){
+        if(err) console.log(err)
+        else res.send("Dodano record")
+    })
+})
+
+app.listen(port, ()=>{
+    console.log("Aplikacja działa na porcie: "+port)
+})
