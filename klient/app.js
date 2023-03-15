f25852fb735f32c8ffa0885d3a8fe27629473e7c
MrBartek302 <126457666+MrBartek302@users.noreply.github.com>
2023-03-15 19:09:53 +0100

    losowanie

--- klient/app.js
+++ klient/app.js
@@ -1,20 +1,15 @@
 var json = []
-
 async function getkandydat(){
     const data = await fetch(baseurl+"/pobierz_nazwe_kandydata")
     json = await data.json()
     console.log(json)
-    
-}
-
+    }
+getkandydat()
 function zagraj(){
-    getkandydat()
-    document.getElementById("prawogra").innerHTML = ""
+    
+    
     document.getElementById("interfejs").innerHTML =""
     document.getElementById("dollewo").innerHTML = ""
-
-
-    for(var i =0; i<= json.length - 1;i++){
 
     const napis1 = document.createElement("h3")
     napis1.innerHTML = "Wpisz swój Pesel"
@@ -35,64 +30,26 @@ function zagraj(){
     buttonwyslij.setAttribute("onclick", "losuj()")
     document.getElementById("dollewo").appendChild(buttonwyslij)
     
-    const div_kandydat1 = document.createElement("div")
-    div_kandydat1.classList.add("uzytkownicy")
-    div_kandydat1.setAttribute("id", "uzytkownik1")
-    const nazwa1 = document.createElement("h1")
-    nazwa1.setAttribute("id", "nazwa1")
-    nazwa1.innerHTML = json[i].nazwa_kandydata
-    div_kandydat1.appendChild(nazwa1)
-    const div1_zdj = document.createElement("img")
-    div1_zdj.setAttribute("src","https://www.ubutik.pl/img/cms/kiedy%20okulary%20dla%20nauczyciela.jpg" )
-    div1_zdj.classList.add("zdjecia")
-    div_kandydat1.appendChild(div1_zdj)
-    const button1 = document.createElement("button")
-    button1.setAttribute("onclick", "wybierz(1)")
-    button1.setAttribute("id", "przycisk1")
-    button1.classList.add("wybieranie")
-    button1.innerHTML = "Wybierz!"
-    div_kandydat1.appendChild(button1)
+    for(var i =0; i<= json.length - 1;i++){
+    const div_kandydat = document.createElement("div")
+    div_kandydat.classList.add("uzytkownicy")
+    const nazwa = document.createElement("h1")
+    nazwa.classList.add("nazwa1")
+    nazwa.innerHTML = json[i].nazwa_kandydata
+    div_kandydat.appendChild(nazwa)
+    const div_zdj = document.createElement("img")
+    div_zdj.setAttribute("src", json[i].zdj_kandydata)
+    div_zdj.classList.add("zdjecia")
+    div_kandydat.appendChild(div_zdj)
+    const button = document.createElement("button")
+    button.setAttribute("onclick", `wybierz(${(i+1)})`)
+    button.classList.add("wybieranie")
+    button.innerHTML = "Wybierz!"
+    div_kandydat.appendChild(button)
 
-    const div_kandydat2 = document.createElement("div")
-    div_kandydat2.classList.add("uzytkownicy")
-    div_kandydat2.setAttribute("id", "uzytkownik1")
-    const nazwa2 = document.createElement("h1")
-    nazwa2.setAttribute("id", "nazwa2")
-    nazwa2.innerHTML = json[i].nazwa_kandydata
-    div_kandydat2.appendChild(nazwa2)
-    const div2_zdj = document.createElement("img")
-    div2_zdj.setAttribute("src"," https://st.depositphotos.com/1637787/3445/i/600/depositphotos_34453787-stock-photo-businessman-portrait.jpg" )
-    div2_zdj.classList.add("zdjecia")
-    div_kandydat2.appendChild(div2_zdj)
-    const button2 = document.createElement("button")
-    button2.setAttribute("onclick", "wybierz(2)")
-    button2.setAttribute("id", "przycisk2")
-    button2.classList.add("wybieranie")
-    button2.innerHTML = "Wybierz!"
-    div_kandydat2.appendChild(button2)
-
-    const div_kandydat3 = document.createElement("div")
-    div_kandydat3.classList.add("uzytkownicy")
-    div_kandydat3.setAttribute("id", "uzytkownik1")
-    const nazwa3 = document.createElement("h1")
-    nazwa3.setAttribute("id", "nazwa3")
-    nazwa3.innerHTML = json[i].nazwa_kandydata
-    div_kandydat3.appendChild(nazwa3)
-    const div3_zdj = document.createElement("img")
-    div3_zdj.setAttribute("src","https://st2.depositphotos.com/2501025/5654/i/450/depositphotos_56545139-stock-photo-password.jpg" )
-    div3_zdj.classList.add("zdjecia")
-    div_kandydat3.appendChild(div3_zdj)
-    const button3 = document.createElement("button")
-    button3.setAttribute("onclick", "wybierz(3)")
-    button3.setAttribute("id", "przycisk3")
-    button3.classList.add("wybieranie")
-    button3.innerHTML = "Wybierz!"
-    div_kandydat3.appendChild(button3)
-
-    document.getElementById("prawogra").appendChild(div_kandydat1)
-    document.getElementById("prawogra").appendChild(div_kandydat2)
-    document.getElementById("prawogra").appendChild(div_kandydat3)
-
+    
+    document.getElementById("prawogra").appendChild(div_kandydat)
+    }
     const napispesel= document.createElement("h2")
     napispesel.innerHTML = "Twój Pesel to: "
     napispesel.setAttribute("id", "napispesel11")
@@ -102,7 +59,7 @@ function zagraj(){
     napiskandydat.innerHTML = "Twój wybór to: "
     napiskandydat.setAttribute("id", "napiskandydat11")
     document.getElementById("interfejs").appendChild(napiskandydat)
-    }
+    
     
 }
 
@@ -110,23 +67,17 @@ function zagraj(){
 let pesel;
 function zapisz(){
     pesel = document.getElementById("pesel").value;
+    document.getElementById("napispesel11").innerHTML = "Twój Pesel to: "+pesel
 }
 var wybrany = 0;
+var nazwakandy;
 function losuj(){
    if(wybrany==0){
-    const napispesel1 = document.getElementById("napispesel11");
-    const napiskandydat1 = document.getElementById("napiskandydat11");
-    
     const peseluzytkownik = pesel;
     const nazwakandy = document.getElementById("nazwakandydata").innerHTML;
-
-    
     const url = `${baseurl}/add/${nazwakandy}/${peseluzytkownik}`;
 
     fetch(url);
-
-    napispesel1.innerHTML = "Twój Pesel to: "+peseluzytkownik;
-    napiskandydat1.innerHTML = "Wybrałeś kandydata: "+nazwakandy;
 
     wybrany = 1
 
@@ -134,10 +85,10 @@ function losuj(){
 }
 getkandydat()
 }
-
 function wybierz(nr){
-    const nazwauzytkownika = document.getElementById("nazwa"+nr)
-    nazwauzytkownika.setAttribute("id", "nazwakandydata")
+    const nazwauzytkownika = document.getElementsByClassName("nazwa1");
+    nazwauzytkownika[i].setAttribute("id", "nazwakandydata")
+    document.getElementById("napiskandydat11").innerHTML = "Twój wybór to: "+json[nr-1].nazwa_kandydata
 
 getkandydat()
 }
